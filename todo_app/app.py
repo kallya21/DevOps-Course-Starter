375746c8a6ee3273bd3e8831ec64265803c5633a
kallya21 <KamiliaAboud@outlook.com>
2023-07-25 23:20:06 +0100

    Added new route to app.py and updated index.html to display move to done buttons

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request, redirect
 
 from todo_app.flask_config import Config
 
-from todo_app.data.session_items import get_items, add_item
-
-from todo_app.data.trello_items import get_trello_items, add_trello_item, get_to_do_list
+from todo_app.data.trello_items import get_trello_items, add_trello_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -12,7 +10,6 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    get_to_do_list()
     cards = get_trello_items()
     return render_template('index.html', cards=cards)
 
@@ -21,3 +18,7 @@ def new_item():
     item = request.form.get('new_item')
     add_trello_item(item)
     return redirect('/')
+
+@app.route('/complete_item', methods=['POST'])
+def complete_item():
+    pass
