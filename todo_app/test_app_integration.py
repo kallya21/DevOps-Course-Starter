13813fca12be8d529182bd697293ecf4517e5491
kallya21 <KamiliaAboud@outlook.com>
2024-04-22 18:10:19 +0100

    Updated test_app_integration.py

--- todo_app/test_app_integration.py
+++ todo_app/test_app_integration.py
@@ -1,44 +1,25 @@
 import os
+import pymongo
 import pytest
+import mongomock
 from dotenv import load_dotenv, find_dotenv
-import requests
 from todo_app import app
+from todo_app.data.db_items import add_todo_item
 
 @pytest.fixture
 def client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
 
-    test_app = app.create_app()
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
 
-    with test_app.test_client() as client:
-        yield client
-
-def test_index_page(monkeypatch, client):
-    # This replaces any call to requests.request with our own function
-    monkeypatch.setattr(requests, 'request', stub)
+def test_index_page(client):
+    add_todo_item("Test item")
 
     response = client.get('/')
 
     assert response.status_code == 200
-    assert 'Test card' in response.data.decode()
-
-class StubResponse():
-    def __init__(self, fake_response_data):
-        self.fake_response_data = fake_response_data
-
-    def json(self):
-        return self.fake_response_data
-
-def stub(method, url, params={}, headers={}):
-    test_board_id = os.environ.get('BOARD_ID')
-
-    if url == f'https://api.trello.com/1/boards/{test_board_id}/lists':
-        fake_response_data = [{
-            'id': '123abc',
-            'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card'}]
-        }]
-        return StubResponse(fake_response_data)
-
-    raise Exception(f'Integration test did not expect URL "{url}"')
+    assert b'Test item' in response.data
