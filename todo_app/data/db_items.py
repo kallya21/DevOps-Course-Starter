282ce1952c78d9353cc0042d89a98f38c3f221d8
kallya21 <KamiliaAboud@outlook.com>
2024-04-22 17:20:01 +0100

    updated methods in db_items, updated app.py and index.html to display data from mongodb

--- todo_app/data/db_items.py
+++ todo_app/data/db_items.py
@@ -2,30 +2,44 @@ import pymongo
 import os
 
 class Item:
-    def __init__(self, name, status = 'To Do'):
+    def __init__(self, _id, name, status = 'To Do'):
+        self._id = _id
         self.name = name
         self.status = status
 
     @classmethod
     def from_db_item(cls, db_item):
-        return cls(db_item['name'], db_item['status'])
+        return cls(db_item['_id'], db_item['name'], db_item['status'])
+    
+def get_primary_connection_string():
+    return os.getenv('PRIMARY_CONNECTION_STRING')
+
+def get_mongo_db_name():
+    return os.getenv('MONGO_DB_NAME')
     
 def get_mongo_client():
-    connection_string = os.getenv('PRIMARY_CONNECTION_STRING')
+    connection_string = get_primary_connection_string()
     client = pymongo.MongoClient(connection_string)
     return client
 
 def get_todo_items():
     client = get_mongo_client()
-    db_name = os.getenv('MONGO_DB_NAME')
+    db_name = get_mongo_db_name()
     db = client[db_name]
     collection = db['todo_items']
-    items = collection.find({})
+    db_itmes = collection.find({})
+
+    items = []
+
+    for db_item in db_itmes:
+        item = Item.from_db_item(db_item)
+        items.append(item)
+
     return items
     
 def add_todo_item(name, status='To Do'):
     client = get_mongo_client()
-    db_name = os.getenv('MONGO_DB_NAME')
+    db_name = get_mongo_db_name()
     db = client[db_name]
     collection = db['todo_items']
     new_item = {'name': name, 'status': status}
@@ -34,7 +48,7 @@ def add_todo_item(name, status='To Do'):
 
 def update_item_status(item_id, new_status='Done'):
     client = get_mongo_client()
-    db_name = os.getenv('MONGO_DB_NAME')
+    db_name = get_mongo_db_name()
     db = client[db_name]
     collection = db['todo_items']
     result = collection.update_one({'_id': item_id}, {'$set': {'status': new_status}})
