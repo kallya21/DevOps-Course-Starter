951ef2eb161d920cb2e3fecea486de08249b0b0b
kallya21 <KamiliaAboud@outlook.com>
2024-05-13 15:54:58 +0100

    Updated main.tf with resources

--- main.tf
+++ main.tf
@@ -14,3 +14,29 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
   name     = "Cohort28_KamAbo_ProjectExercise"
 }
+
+resource "azurerm_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name                = "terraformed-ka-todoapp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    application_stack {
+      docker_image     = "appsvcsample/python-helloworld"
+      docker_image_tag = "latest"
+    }
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+  }
+}
