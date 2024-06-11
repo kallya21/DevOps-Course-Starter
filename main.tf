764f862e7c084a5fc325d5361a98bbecd89e2568
kallya21 <KamiliaAboud@outlook.com>
2024-06-11 15:40:24 +0100

    updated main.tf to create db

--- main.tf
+++ main.tf
@@ -31,12 +31,55 @@ resource "azurerm_linux_web_app" "main" {
 
   site_config {
     application_stack {
-      docker_image     = "appsvcsample/python-helloworld"
+      docker_image     = "kallya21/todo-app"
       docker_image_tag = "latest"
     }
   }
 
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+    "SECRET_KEY" = "secret-key"
+    "PRIMARY_CONNECTION_STRING" = azurerm_cosmosdb_account.main.primary_mongodb_connection_string
+    "MONGO_DB_NAME" = "todo-app-db"
+    "FLASK_APP" = "todo_app/app"
+    "WEBSITES_PORT" = "8000"
+  }
+}
+
+resource "azurerm_cosmosdb_account" "main" {
+  name                 = "terraformed-todo-app-mongo-account"
+  location             = data.azurerm_resource_group.main.location
+  resource_group_name  = data.azurerm_resource_group.main.name
+  offer_type           = "Standard"
+  kind                 = "MongoDB"
+  mongo_server_version = "4.2"
+
+  capabilities {
+    name = "EnableServerless"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "uksouth"
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "main" {
+  name                = "terraformed-todo-app-db"
+  resource_group_name = azurerm_cosmosdb_account.main.resource_group_name
+  account_name        = azurerm_cosmosdb_account.main.name
+
+  lifecycle { 
+    prevent_destroy = true 
   }
 }
