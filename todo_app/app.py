d086e6b0c7aaa5cebfe0d820912a6f8eec9fe01e
kallya21 <KamiliaAboud@outlook.com>
2024-07-20 03:42:11 +0100

    configured app to send logs to loggly

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
 from todo_app.data.db_items import get_todo_items, add_todo_item, update_item_status
 from todo_app.data.view_model import ViewModel
-import logging
+from loggly.handlers import HTTPSHandler
+from logging import Formatter
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
     app.logger.setLevel(app.config['LOG_LEVEL'])
+
+    if app.config['LOGGLY_TOKEN'] is not None:
+        handler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{app.config["LOGGLY_TOKEN"]}/tag/todo-app')
+        handler.setFormatter(
+            Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+        )
+        app.logger.addHandler(handler)
 
     @app.route('/')
     def index():
@@ -20,7 +28,7 @@ def create_app():
     def new_item():
         item = request.form.get('new_item')
         add_todo_item(item)
-        app.logger.info("Successfully added To Do item: %s", item)
+        app.logger.info("Successfully added To Do item")
         return redirect('/')
 
     @app.route('/complete_item/<item_id>', methods=['POST'])
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -9,3 +9,7 @@ class Config:
             raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
         
         self.LOG_LEVEL = os.environ.get('LOG_LEVEL')
+
+        self.LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
+        if not self.LOGGLY_TOKEN:
+            raise ValueError("No LOGGLY_TOKEN set for Flask application. Did you follow the setup instructions?")
